d36b1ed14c9f9020d1f834b9c1f2d97d6e74299a
ebarross <emanoel.oliveira@ccc.ufcg.edu.br>
2015-01-30 01:52:43 -0300

    Tela de Login.

---
+++ HotelRiviera/src/hotel/Acesso.java
@@ -0,0 +1,49 @@
+package hotel;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+
+import com.mysql.jdbc.Statement;
+
+public class Acesso {
+	
+public boolean acesso;
+    
+    public void Acesso(String login, String senha){
+       Connection con = null;
+       Statement consulta = null;
+       ResultSet tabela = null;
+       
+       try{
+         Class.forName("com.mysql.jdbc.Driver");
+         con = DriverManager.getConnection("jdbc:mysql://localhost/login", "root", "");
+         consulta = (Statement) con.createStatement();
+         tabela = consulta.executeQuery("select login, senha from tabela_usuario where login='"+login+"'and senha='"+senha+"'");
+			if (tabela.next()) {
+				JOptionPane.showMessageDialog(null, "Bem vindo!");
+				acesso = true;
+			} else {
+				JOptionPane.showMessageDialog(null, "            Acesso negado!\nUsuário ou Senha incorreto");
+				acesso = false;
+			}
+			
+			
+			
+			
+		}catch(ClassNotFoundException |SQLException e){
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ HotelRiviera/src/hotel/Login.java
@@ -0,0 +1,112 @@
+package hotel;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.text.ParseException;
+
+public class Login extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtLogin;
+	private JPasswordField txtSenha;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login frame = new Login();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Login() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		JLabel lblNewLabel = new JLabel("Login:");
+		lblNewLabel.setBounds(139, 126, 46, 14);
+		contentPane.add(lblNewLabel);
+
+		JLabel lblNewLabel_1 = new JLabel("Senha:");
+		lblNewLabel_1.setBounds(139, 151, 46, 14);
+		contentPane.add(lblNewLabel_1);
+
+		txtLogin = new JTextField();
+		txtLogin.setBounds(183, 120, 86, 20);
+		contentPane.add(txtLogin);
+		txtLogin.setColumns(10);
+
+		txtSenha = new JPasswordField();
+		txtSenha.setBounds(183, 145, 86, 20);
+		contentPane.add(txtSenha);
+
+		JButton btnOK = new JButton("OK");
+		btnOK.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+
+				Acesso cl = new Acesso();
+				cl.Acesso(txtLogin.getText(), txtSenha.getText());
+				if (cl.acesso == true) {
+					HotelJ tl = null;
+					try {
+						tl = new HotelJ();
+					} catch (ParseException e) {
+						e.printStackTrace();
+					}
+					tl.show();
+					try {
+						tl.setExtendedState(new HotelJ().MAXIMIZED_BOTH);
+					} catch (ParseException e) {
+						e.printStackTrace();
+					}
+					dispose();
+				}
+				if (cl.acesso == false) {
+					txtLogin.setText("");
+					txtSenha.setText("");
+					txtLogin.requestFocus();
+				}
+
+				cl.acesso = false;
+
+			}
+		});
+		btnOK.setBounds(109, 187, 89, 23);
+		contentPane.add(btnOK);
+
+		JButton btnCancelar = new JButton("Cancelar");
+		btnCancelar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.exit(0);
+
+			}
+		});
+		btnCancelar.setBounds(208, 187, 89, 23);
+		contentPane.add(btnCancelar);
+	}
+}
