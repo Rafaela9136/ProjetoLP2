e3a9d34057b23288cf4b970fa93cf18bf10e5f1f
ebarross <emanoel.oliveira@ccc.ufcg.edu.br>
2015-01-31 03:03:22 -0300

    É melhor deixar pra fazer a classe de testes da Contrato depois que fizer as outras.

--- HotelRiviera/src/testes/ContratoTest.java
+++ HotelRiviera/src/testes/ContratoTest.java
@@ -14,6 +14,7 @@ import hotel.TiposQuartosExecutivo;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import org.junit.Assert;
@@ -21,7 +22,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class ContratoTest {
-	
+
 	private Contrato contrato1, contrato2;
 	private Calendar c = Calendar.getInstance();
 	private Hospede hospedeTitular;
@@ -29,20 +30,26 @@ public class ContratoTest {
 	private List<String> hospedes = new ArrayList();
 	private List<Quarto> quartos = new ArrayList();
 	private List<Servico> servicos = new ArrayList();
-//
-//	@Before
-//	public void criaObjetos() throws Exception{
-//		hospedeTitular = new  Hospede("nome", 10, 9, 1996);
-//		hospedes.add("Maria Cocielo");
-//		quarto = new QuartoExecutivo(false, TiposQuartosExecutivo.EXECUTIVO_SIMPLES);
-//		quartos.add(quarto);
-//		contrato1 = new Contrato(hospedeTitular, hospedes, Estrategias.NATAL_REVEILION, 10, 6, 2015, 20, 6, 2015, false, quartos, servicos);
-//		contrato2 = new Contrato(hospedeTitular, hospedes, Estrategias.SAO_JOAO, 15, 3, 2015, 23, 3, 2015, true, quartos, servicos);
-//	}
-//	
+
+	//
+	// @Before
+	// public void criaObjetos() throws Exception{
+	// hospedeTitular = new Hospede("nome", 10, 9, 1996);
+	// hospedes.add("Maria Cocielo");
+	// quarto = new QuartoExecutivo(false,
+	// TiposQuartosExecutivo.EXECUTIVO_SIMPLES);
+	// quartos.add(quarto);
+	// contrato1 = new Contrato(hospedeTitular, hospedes,
+	// Estrategias.NATAL_REVEILION, 10, 6, 2015, 20, 6, 2015, false, quartos,
+	// servicos);
+	// contrato2 = new Contrato(hospedeTitular, hospedes, Estrategias.SAO_JOAO,
+	// 15, 3, 2015, 23, 3, 2015, true, quartos, servicos);
+	// }
+	//
 	@Test
 	public void testEditarServicos() {
-		Servico carro = new Carro(TipoCarro.EXECUTIVO, true, true);
+		Servico carro = new Carro(TipoCarro.EXECUTIVO, new GregorianCalendar(
+				2014, 1, 5), true, true);
 		for (int i = 0; i < 5; i++) {
 			contrato1.adicionaServico(carro);
 		}
