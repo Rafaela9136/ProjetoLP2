330812e5f3dece25b449636c39296fc057fdeade
ebarross <emanoel.oliveira@ccc.ufcg.edu.br>
2015-02-02 01:07:26 -0300

    A tela de login tava ficando aberta quando abria o programa ;p

--- HotelRiviera/src/interfaceGrafica/LoginJ.java
+++ HotelRiviera/src/interfaceGrafica/LoginJ.java
@@ -34,7 +34,8 @@ public class LoginJ extends JFrame {
 	 */
 	public static void main(String[] args) {
 		try {
-			UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+			UIManager
+					.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
 		} catch (Throwable e) {
 			e.printStackTrace();
 		}
