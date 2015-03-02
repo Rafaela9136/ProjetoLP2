097770ae184790e54e58296aa50ec6f9a771de86
Rafael <Rafael@pc-Rafael.home>
2015-03-02 19:44:43 -0300

    e

--- HotelRiviera/src/hotel/IndexQuartos.java
+++ HotelRiviera/src/hotel/IndexQuartos.java
@@ -3,17 +3,23 @@ package hotel;
 import java.io.Serializable;
 
 public enum IndexQuartos implements Serializable {
-	EXECUTIVO_SIMPLES(5), EXECUTIVO_DUPLO(15), EXECUTIVO_TRIPLO(20),
-	LUXO_SIMPLES(5), LUXO_DUPLO(15), LUXO_TRIPLO(20),
-	SUITE_PRESIDENCIAL(5);
+	EXECUTIVO_SIMPLES(5, "Executivo Simples"), EXECUTIVO_DUPLO(15, "Executivo Duplo"), EXECUTIVO_TRIPLO(20, "Executivo Triplo"),
+	LUXO_SIMPLES(5, "Luxo Simples"), LUXO_DUPLO(15, "Luxo Duplo"), LUXO_TRIPLO(20, "Luxo Triplo"),
+	SUITE_PRESIDENCIAL(5,"Suite Presidencial");
 	
 	private int quantidade;
+	private String nome;
 	
-	private IndexQuartos(int quantidade) {
+	private IndexQuartos(int quantidade, String nome) {
 		this.quantidade = quantidade;
+		this.nome = nome;
 	}// construtor
 	
 	public int getQuantidade() {
 		return quantidade;
 	}// getQuantidade
+	
+	public String getNome() {
+		return nome;
+	}// getNome
 }// IndexQuartosDesocupados
